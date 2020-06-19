0a169c896bb117f64d12025592a7cba4b25442cc
MotiRR <motigullin.ra@gmail.com>
2020-06-19 13:46:00 +0300

    Delete type of method int[] or int[][] Delete overload method printOnConsole Changed 10, 11 task

--- src/main/java/org/vtb/lesson1/MainApp.java
+++ src/main/java/org/vtb/lesson1/MainApp.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class MainApp {
-    
+
     //    1. Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20
     //    (включительно), если да – вернуть true, в противном случае – false.
     public static boolean computeSum(int first, int second) {
